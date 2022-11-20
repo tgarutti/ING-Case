b73c8586004fa3ef7a53969c73f23002a4b3582b
xhuliahoxha <118306099+xhuliahoxha@users.noreply.github.com>
2022-11-20 19:26:55 +0100

    delinquency rates and QoQ  plots

--- main.py
+++ main.py
@@ -7,73 +7,82 @@ Created on Sun Nov 13 14:30:37 2022
 """
 
 # %% Import packages
-import source.importData as importData
-import source.statisticalTests as statTests
 import numpy as np
 import pandas as pd
 from datetime import datetime
+
+import source.importData as importData
+import source.statisticalTests as statTests
 import source.varModel as varM
 import source.varmsModel as varmsM
+import source.plots as plots
 
 # %% Read and process raw data
 data_raw, data_dict = importData.readData()
-#importData.plotAllData(data_dict)
+#plots.plotAllData(data_dict)
 
 delinquency = data_dict['delinquency']
 historical = data_dict['historical'].drop(['date_from'],axis=1)
 scenarios = data_dict['scenarios'].drop(['date_from'],axis=1)
 
-endog = importData.transformData(delinquency, 'diff4_diff1')
+plots.plotDelinquencyRates(delinquency)
+
+endog = importData.transformData(delinquency, 'diff4')
+plots.plotQoQDelinquencyRates(endog)
+
+
 exog = importData.transformData(historical[historical.columns[5:]], 'diff1')
 exog.columns = historical.columns[5:]
 
 endog, exog, iidx = importData.matchIdx(endog, exog, lag=1)
 
+
+
 #aDF, granger_results = statTests.testNonStationarity(endogQoQ, covariates=exogQoQ)
 
-# %% Fit vector autoregressive (VAR) model and check out of sample performance
-varData = varM.trainTestData(endog, exog, 12)
+# # %% Fit vector autoregressive (VAR) model and check out of sample performance
+# varData = varM.trainTestData(endog, exog, 12)
 
 
-results = {}
+# results = {}
 
-empty = pd.DataFrame()
-results['VAR (1)'] = varM.varModel(varData, 1, 0, 1, X = 0)
-results['AR (1)'] = varM.varModel(varData, 1, 0, 0, X = 0)
+# empty = pd.DataFrame()
+# results['VAR (1)'] = varM.varModel(varData, 1, 0, 1, X = 0)
+# results['AR (1)'] = varM.varModel(varData, 1, 0, 0, X = 0)
 
-results['VARX (1)'] = varM.varModel(varData, 1, 0, 1, X = 1)
-results['ARX (1)'] = varM.varModel(varData, 1, 0, 0, X = 1)
+# results['VARX (1)'] = varM.varModel(varData, 1, 0, 1, X = 1)
+# results['ARX (1)'] = varM.varModel(varData, 1, 0, 0, X = 1)
 
-# importData.plotDF(forecasts, 'Delinquency (QoQ) Forecasts')
-# importData.plotDF(test_endog, 'Delinquency (QoQ)')
-
-
-# %% Fit the VAR Markov Switching model and check out of sample performance
-
-varMSData = varmsM.trainTestData(endog, exog, 1, 12)
-
-varmsHP = {}
-varmsHP['trend'] = 'n'
-varmsHP['var'] = True
-varmsHP['ar'] = True
-varmsHP['exog'] = True
-varmsHP['trend_switch'] = False
+# # importData.plotDF(forecasts, 'Delinquency (QoQ) Forecasts')
+# # importData.plotDF(test_endog, 'Delinquency (QoQ)')
 
 
-empty = pd.DataFrame()
-#results['ARMS (1)'] = varmsM.varMSModel(varMSData, 1, 0, X = 0, hyperparams= varmsHP)
+# # %% Fit the VAR Markov Switching model and check out of sample performance
 
-results['ARXMS (1)'] = varmsM.varMSModel(varMSData, 1, 0, X = 1, hyperparams= varmsHP)
+# varMSData = varmsM.trainTestData(endog, exog, 1, 12)
 
-# %% Create MSE matrix
-MSE = pd.DataFrame()
-colnames = []
-for key in results.keys():
-    MSE = pd.concat([MSE,(results[key])['MSE']],axis=1)
-    colnames.append(key)
-MSE.columns = colnames
-MSE = MSE.T
+# varmsHP = {}
+# varmsHP['trend'] = 'n'
+# varmsHP['var'] = True
+# varmsHP['ar'] = True
+# varmsHP['exog'] = True
+# varmsHP['trend_switch'] = False
 
-# Divide by benchmark
-MSE_comparison = MSE/MSE.loc['ARX (1)']
-MSE_weighted = MSE/abs(varMSData['test_endog'].mean(axis=0))
+
+# empty = pd.DataFrame()
+# #results['ARMS (1)'] = varmsM.varMSModel(varMSData, 1, 0, X = 0, hyperparams= varmsHP)
+
+# results['ARXMS (1)'] = varmsM.varMSModel(varMSData, 1, 0, X = 1, hyperparams= varmsHP)
+
+# # %% Create MSE matrix
+# MSE = pd.DataFrame()
+# colnames = []
+# for key in results.keys():
+#     MSE = pd.concat([MSE,(results[key])['MSE']],axis=1)
+#     colnames.append(key)
+# MSE.columns = colnames
+# MSE = MSE.T
+
+# # Divide by benchmark
+# MSE_comparison = MSE/MSE.loc['ARX (1)']
+# MSE_weighted = MSE/abs(varMSData['test_endog'].mean(axis=0))
---
+++ source/plots.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+
+# NBER recessions
+from pandas_datareader.data import DataReader
+from datetime import datetime
+
+# %% Plot data
+def plotDelinquencyRates(delinquency):
+    plt.figure()
+    plt.plot(delinquency)
+    plt.ylabel('delinquency rate')
+    plt.legend(['All real estate', 'All customer', 'Leases', 'C&I', 'Agricultural'])
+
+    usrec = DataReader('USREC', 'fred', start=datetime(1987, 1, 1), end=datetime(2022, 4, 1))
+    #plt.title('Delinquency Rates (NSA) from 1987 to 2022 for all commercial banks')
+    plt.fill_between(usrec.index, 0, 1, where=usrec['USREC'].values, color='k', alpha=0.1)
+    plt.ylim([0,0.12])
+
+
+def plotQoQDelinquencyRates(delinquency_QoQ):
+    plt.figure()
+    plt.plot(delinquency_QoQ)
+    plt.ylabel('QoQ delinquency rate')
+    plt.legend(['All real estate', 'All customer', 'Leases', 'C&I', 'Agricultural'])
+
+    usrec = DataReader('USREC', 'fred', start=datetime(1987, 1, 1), end=datetime(2022, 4, 1))
+    #plt.title('Delinquency Rates (NSA) from 1987 to 2022 for all commercial banks')
+    plt.fill_between(usrec.index, -1, 1, where=usrec['USREC'].values, color='k', alpha=0.1)
+    plt.ylim([-0.04,0.05])
+
+
+def plotAllData(data):
+    plotData(data, 'chargeoff')
+    plotData(data, 'delinquency')
+    plotHistoricalData(data['historical'])
+    plotScenarioData(data['scenarios'])
+
+
+def plotData(data, data_name):
+    sub_data = data[data_name]
+    col_names = sub_data.columns
+    ignore_cols = {'residential', 'commercial', 'farmland', 
+                   'credit_card', 'other', 'total'}
+    col_list = [col for col in col_names if col not in ignore_cols]
+    plt.figure()
+    plt.plot(sub_data[col_list])
+    plt.legend(col_list)  
+    plt.title(data_name)
+        
+
+def plotHistoricalData(sub_data):
+    col_names = sub_data.columns
+    fig, axs = plt.subplots(5,2, figsize=(20, 20))
+    k = 0
+    for j in range(2):
+        for i in range(5):
+            axs[i,j].plot(sub_data[col_names[k+1]])
+            axs[i,j].title.set_text(col_names[k+1])
+            k = k + 1
+    
+def plotScenarioData(sub_data):
+    col_names = sub_data.columns
+    fig, axs = plt.subplots(5, figsize=(10,20))
+    for i in range(5):
+        axs[i].plot(sub_data[col_names[i+1]])
+        axs[i].title.set_text(col_names[i+1])
+        
+def plotDF_subplots(df):
+    col_names = df.columns
+    fig, axs = plt.subplots(len(col_names), figsize=(30,20))
+    for i in range(len(col_names)):
+        axs[i].plot(df[col_names[i]])
+        axs[i].title.set_text(col_names[i])
+        
+def plotDF(df, str_title):
+    col_names = df.columns
+    plt.figure()
+    plt.plot(df)
+    plt.legend(col_names)  
+    plt.title(str_title)
